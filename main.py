8c9e0bd1f6cf9151862cf5fc074b81fd63b091e1
Estrella Adriana Sicardi Segade <esicardi@gmail.com>
2024-07-13 18:25:40 -0300

    actualizacion

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-
-
+from databricks.connect import DatabricksSession as SparkSession
+from databricks.sdk.core import Config
+from utils import read_yaml
 
 if __name__ =='__main__':
-    from databricks.connect import DatabricksSession as SparkSession
-    from databricks.sdk.core import Config
 
+
+    # 1) Incializacion de spark
     config =  Config(profile="DEFAULT", cluster_id="0709-232718-y2zn1btt")
     spark = SparkSession.builder.sdkConfig(config).getOrCreate()
 
-    spark.sql("show databases").show()
+    # 2) yaml con todos los parametros
+    config=read_yaml()
